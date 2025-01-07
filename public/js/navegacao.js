65f208e191acd41b18186bc8c5e9cf6a19eb138a
Yasser Abdallah <yassercondoluci@hotmail.com>
2025-01-07 14:20:53 -0400

    Home page pronto

---
+++ public/js/navegacao.js
@@ -0,0 +1,12 @@
+document.getElementById("a").addEventListener("click", function () {
+    document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("a").addEventListener("click", function () {
+    document.getElementById("servicos").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("a").addEventListener("click", function () {
+    document.getElementById("contato").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("a").addEventListener("click", function () {
+    document.getElementById("sobre").scrollIntoView({ behavior: "smooth" });
+});
