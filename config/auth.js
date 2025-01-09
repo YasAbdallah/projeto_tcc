2e60f9c0fcc31b8ec8c310b62b8267d30a5de7d3
Yasser Abdallah <yassercondoluci@hotmail.com>
2025-01-09 16:03:26 -0400

    Adiciona modelo de Barbeiro e atualiza estilos CSS, além de remover arquivos obsoletos do Font Awesome.

--- config/auth.js
+++ config/auth.js
@@ -1,38 +1,76 @@
-const LocalStrategy = require('passport-local').Strategy
-const mongoose = require('mongoose')
-const bcrypt = require("bcryptjs")
-require("../models/Usuario")
-const Usuario = mongoose.model('usuario')
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
-module.exports = function(passport) {
-    passport.use(new LocalStrategy({usernameField: "cpf", passwordField: "senha"}, (cpf, senha, done) => {
-        Usuario.findOne({cpf: cpf}).then((usuario) => {
-            if(!usuario){ return done(null, false, {message: "Usuário ou senha incorretos."}) }
+require("../models/Cliente");
+const Cliente = mongoose.model('cliente');
+
+require("../models/Barbeiro");
+const Barbeiro = mongoose.model('barbeiro');
+
+module.exports = function (passport) {
+    passport.use(new LocalStrategy({ usernameField: "email", passwordField: "senha" }, (email, senha, done) => {
+        // Verifica no modelo Cliente
+        Cliente.findOne({ email: email }).then((usuario) => {
+            if (!usuario) {
+                // Se não encontrar no modelo Cliente, busca no modelo Barbeiro
+                return Barbeiro.findOne({ email: email }).then((barbeiro) => {
+                    if (!barbeiro) {
+                        return done(null, false, { message: "Usuário ou senha incorretos." });
+                    }
+                    // Comparação de senha para Barbeiro
+                    bcrypt.compare(senha, barbeiro.senha, (err, iguais) => {
+                        if (iguais) {
+                            return done(null, barbeiro);
+                        } else {
+                            return done(null, false, { message: "Usuário ou senha incorretos." });
+                        }
+                    });
+                });
+            }
+            // Comparação de senha para Cliente
             bcrypt.compare(senha, usuario.senha, (err, iguais) => {
-                if(iguais){
-                    return done(null, usuario)
-                }else{
-                    return done(null, false, {message: "Usuário ou senha incorretas."})
+                if (iguais) {
+                    return done(null, usuario);
+                } else {
+                    return done(null, false, { message: "Usuário ou senha incorretos." });
                 }
-            })
-        })
-    }))
+            });
+        }).catch((err) => done(err));
+    }));
 
     passport.serializeUser((user, done) => {
-        return done(null, user._id)
-    })
+        done(null, { id: user._id, tipo: user.tipoUsuario }); // Armazena o tipo de usuário
+    });
 
-    passport.deserializeUser((id, done) => {
-        Usuario.findById(id).then((usuario) => {
-            done(null, {
-                id: usuario.id,
-                nome: usuario.nome,
-                sobrenome: usuario.sobrenome,
-                tipoUsuario: usuario.tipoUsuario,
-                secao: usuario.secao,
-            })
-        })
-    })
-
-
-}
+    passport.deserializeUser((user, done) => {
+        if (user.tipo === 'cliente') {
+            // Busca no modelo Cliente
+            Cliente.findById(user.id).then((usuario) => {
+                if (usuario) {
+                    done(null, {
+                        id: usuario.id,
+                        nome: usuario.nome,
+                        secao: usuario.secao,
+                    });
+                } else {
+                    done(null, false);
+                }
+            }).catch((err) => done(err));
+        } else if (user.tipo === 'barbeiro') {
+            // Busca no modelo Barbeiro
+            Barbeiro.findById(user.id).then((barbeiro) => {
+                if (barbeiro) {
+                    done(null, {
+                        id: barbeiro.id,
+                        nome: barbeiro.nome,
+                    });
+                } else {
+                    done(null, false);
+                }
+            }).catch((err) => done(err));
+        } else {
+            done(null, false);
+        }
+    });
+};
--- models/Barbeiro.js
+++ models/Barbeiro.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
 const Barbeiro = new Schema({
     nome: {
         type: String,
