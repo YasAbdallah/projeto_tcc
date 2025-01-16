bbc09b531e366deaa778f9adecee48d2c22547c0
Yasser Abdallah <yassercondoluci@hotmail.com>
2025-01-16 08:52:45 -0400

    Ajusta o comportamento de rolagem e adiciona margens de rolagem para seções da página

--- public/js/navegacao.js
+++ public/js/navegacao.js
@@ -1,16 +1,10 @@
-document.onload = () => {
-    navegacao("servicos")
-    navegacao("contato")
-    navegacao("sobre")
-}
-
-
 const navegacao = (topico) => {
     document.getElementById("a").addEventListener("click", function () {
         const target = document.getElementById(topico);
-        const offsetTop = target.getBoundingClientRect().top + window.scrollY;
+        const offset = 130; // Ajuste o valor do deslocamento conforme necessário
+        const position = target.getBoundingClientRect().top + window.scrollY - offset;
         window.scrollTo({
-            top: offsetTop,
+            top: position,
             behavior: "smooth"
         });
     })
