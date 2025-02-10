5e709c3b778db20d16bfd6bb810bc545f7d2a78f
Yasser Abdallah <yassercondoluci@hotmail.com>
2025-02-10 16:17:24 -0400

    Ajusta formatação e espaçamento no script de validação de agendamento, melhorando a legibilidade do código.

--- public/js/painel/validarAgenda.js
+++ public/js/painel/validarAgenda.js
@@ -1,4 +1,4 @@
-import {popup} from "../funcoes.js";
+import { popup } from "../funcoes.js";
 
 const selectionBarbeiro = document.getElementById('barbeiro')
 const selectionServico = document.getElementById('servico')
@@ -12,12 +12,9 @@ window.onload = async () => {
     await validarHorario();
 }
 
-
-
 const selectBarbeiro = async () => {
     selectionBarbeiro.addEventListener('change', function () {
         const selectedOption = this.options[this.selectedIndex];
-    
         // Atualiza os serviços
         const servicos = selectedOption.getAttribute('data-servicos').split(',');
         selectionServico.innerHTML = '<option value="" disabled selected>Selecione o serviço desejado.</option>';
@@ -38,12 +35,11 @@ const validarDia = async () => {
     selectionData.addEventListener('change', function () {
         const selectBarbeiro = document.getElementById('barbeiro');
         const selectedOption = selectBarbeiro.options[selectBarbeiro.selectedIndex];
-
         const dataSelecionada = new Date(document.getElementById("data").value);
         const diaSelecionado = dataSelecionada.getDay() + 1;
         const diasDisponiveis = selectedOption.getAttribute('data-dias');
         if (!diasDisponiveis.includes(diaSelecionado)) {
-            popup(popupErro, {sucesso: false, message: 'O barbeiro não trabalha nesse dia. Por favor, escolha outro dia.'});
+            popup(popupErro, { sucesso: false, message: 'O barbeiro não trabalha nesse dia. Por favor, escolha outro dia.' });
             this.value = '';
         }
     });
@@ -54,16 +50,13 @@ const diasDaSemana = (dias) => {
     return dias.map(dia => diasDaSemana[dia]);
 }
 
-
 const validarHorario = async () => {
-    selectionHorario.addEventListener('change', function() {
+    selectionHorario.addEventListener('change', function () {
         const tempoSelecionado = this.value;
-    
         const horas = parseInt(tempoSelecionado.split(':')[0], 10);
-        
         // Verifica se o horário está entre 9h e 18h
         if (horas < 9 || horas >= 18) {
-            popup(popupErro, {sucesso: false, message: 'Os barbeiros só trabalham entre 9h às 13h e 14h às 18h. Por favor, escolha um horário dentro desse intervalo.'});
+            popup(popupErro, { sucesso: false, message: 'Os barbeiros só trabalham entre 9h às 13h e 14h às 18h. Por favor, escolha um horário dentro desse intervalo.' });
             this.value = '';
         }
     });
