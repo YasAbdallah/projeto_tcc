fd94201686b756cbdbf4dfca70011f426781eb20
Yasser Abdallah <yassercondoluci@hotmail.com>
2025-02-13 16:43:42 -0400

    Adiciona novas rotas para agendamentos, histórico e perfil no painel do funcionário. Atualiza a exibição de agendamentos e implementa a lógica para exibir a data atual. Remove a barra lateral do layout e ajusta o estilo do painel.

---
+++ public/js/painel/funcionario/validacao.js
@@ -0,0 +1,11 @@
+
+window.onload = async () => {    
+    await dataAtual()
+}   
+
+const dataAtual = async () => {
+    const spanDataAtual = document.getElementById('dataAtual');
+    const pegaDataAtual = new Date().toLocaleDateString()
+    return spanDataAtual.innerText = pegaDataAtual
+}
+
--- routes/painel/funcionario.js
+++ routes/painel/funcionario.js
@@ -16,4 +16,21 @@ router.get('/', (req, res) => {
     res.render("painel/funcionario")
 })
 
+router.get('/agenda', (req, res) => {
+    const barbeiros = Barbeiro.find().then((barbeiros) => {
+        res.render('painel/funcionario/agendar', {barbeiros: barbeiros})
+    }).catch((err) => { 
+        req.flash('error_msg', 'Houve um erro ao listar os barbeiros')
+        res.redirect('/painel/funcionario')
+    })
+})
+
+router.get('/historico', (req, res) => {
+    res.render('painel/funcionario/historico')
+})
+
+router.get('/perfil', (req, res) => {
+    res.render('painel/funcionario/perfil')
+})
+
 module.exports = router
